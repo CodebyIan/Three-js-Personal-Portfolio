66af8cfb698d50501581acd218c2e15272f17d1a
Ian Mucheru <imucheru.im@gmail.com>
2023-12-15 18:41:00 +0300

    Navigation Bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+// Imports
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+
+const Navbar = () => {
+  // useState Hooks
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  // Navigation Bar
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary border-b border-gray-500`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        {/* Logo Icon + Logo Name*/}
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src={logo}
+            alt="logo"
+            className="w-9 h-9 object-contain rounded-full"
+          />
+          <p className="text-white tex-[18px] font-bold cursor-pointer flex">
+            Ian Mucheru &nbsp;
+            <span className="sm:block hidden">| Portfolio</span>
+          </p>
+        </Link>
+
+        {/* Big Screen Navigation Bar */}
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((navLink) => (
+            <li
+              key={navLink.id}
+              className={`${
+                active === navLink.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(navLink.title)}
+            >
+              <a href={`#${navLink.id}`}>{navLink.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        {/* Small Screen Navigation Bar */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          {/* Menu For Small Screen */}
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex flex-col justify-start items-start gap-4 ">
+              {navLinks.map((navLink) => (
+                <li
+                  key={navLink.id}
+                  className={`${
+                    active === navLink.title ? "text-white" : "text-secondary"
+                  } font-poppins  font-medium text-[16px] cursor-pointer`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(navLink.title);
+                  }}
+                >
+                  <a href={`#${navLink.id}`}>{navLink.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
